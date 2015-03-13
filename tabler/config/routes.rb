1f41b882890119fc49091f5e92bfcbedc5ff5b0f
mwinterfeld <michael.winterfeld@colorado.edu>
2015-03-13 01:01:11 -0600

    Switched to devise for user modeling and login

--- tabler/config/routes.rb
+++ tabler/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
 
   resources :reservations
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :controller => 'static', :action => 'welcome'
+  #root :controller => 'static', :action => 'welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
