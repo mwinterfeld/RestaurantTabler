74cf141dea44efb7207139308b8068d51b126f05
boyuhan <boha4482@colorado.edu>
2015-03-19 13:29:14 -0600

    Removed restaurant_id from table

--- tabler/db/schema.rb
+++ tabler/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319190318) do
+ActiveRecord::Schema.define(version: 20150319192336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20150319190318) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "reservation_id"
-    t.integer  "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
