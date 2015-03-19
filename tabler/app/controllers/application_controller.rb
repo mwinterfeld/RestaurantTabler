34e8485dbe8a894bd2d8346763ec2863f23fd75c
mwinterfeld <michael.winterfeld@colorado.edu>
2015-03-19 13:16:38 -0600

    Now redirects after login based on admin status

--- tabler/app/controllers/application_controller.rb
+++ tabler/app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(user)
+  	if is_admin(user)
+  		reservations_path  		
+  	else
+  		restaurants_path
+  	end
+  end
+
+  private
+
+  def is_admin(user)
+  	User.find(user.id).isAdmin
+  end
 end
