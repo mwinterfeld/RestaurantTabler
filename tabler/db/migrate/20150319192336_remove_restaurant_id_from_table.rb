4fa461dd518ce8845def941f17ef26a1aeb64298
mwinterfeld <michael.winterfeld@colorado.edu>
2015-03-19 15:12:13 -0600

    Deleted migrations because I manually changed the schema

--- tabler/db/migrate/20150319192336_remove_restaurant_id_from_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRestaurantIdFromTable < ActiveRecord::Migration
-  def change
-  	remove_column :tables, :restaurant_id, :integer
-  end
-end
--- tabler/db/schema.rb
+++ tabler/db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150319202801) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150319202801) do
     t.string   "name"
     t.string   "category"
     t.string   "description"
+    t.integer  "admin_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20150319202801) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "reservation_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
