fff732cb5b994213527626ef2ab535620bbe70e1
Greg Wahl <wahlg@colorado.edu>
2015-03-24 20:35:02 -0600

    Validate restaurant name and category are present

--- tabler/app/models/restaurant.rb
+++ tabler/app/models/restaurant.rb
@@ -1,6 +1,13 @@
 class Restaurant < ActiveRecord::Base
+	# For attached logo image
 	has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ":style/missing.png"
 	validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
+	
+	# Validations
+	validates :name, :presence => true
+	validates :category, :presence => true
+
+	# Associations
 	has_many :reservations
 	has_many :tables, through: :reservations
 end
