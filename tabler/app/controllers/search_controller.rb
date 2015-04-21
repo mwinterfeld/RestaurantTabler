c043988196e3a30913c8f9e23bf7e7e4cc3014f1
gpoisoned <gpoisoned@gmail.com>
2015-04-21 13:08:29 -0600

    Searching restaurant now works

--- tabler/app/controllers/search_controller.rb
+++ tabler/app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
 	def search
 		puts params.inspect
+		@search_results = PgSearch.multisearch(params[:query]) unless params[:query].empty?
 	end
 
 	def index
