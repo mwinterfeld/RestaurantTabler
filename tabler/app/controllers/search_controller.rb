369dcaefb39d969fe1b7bb38c4d6d6dd3f48c6cd
gpoisoned <gpoisoned@gmail.com>
2015-04-21 17:15:25 -0600

    Bug Fixes:
    - Changes devise setting to use different views based on the model
    - Fixes the check of search parameter for empty string
    - Changes layout a bit

--- tabler/app/controllers/search_controller.rb
+++ tabler/app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 	def search
-		@search_results = PgSearch.multisearch(params[:query]) unless params[:query].empty?
+		@search_results = PgSearch.multisearch(params[:query]) unless params[:query].nil?
 	end
 
 	def index
--- tabler/app/controllers/users/registrations_controller.rb
+++ tabler/app/controllers/users/registrations_controller.rb
@@ -1,12 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
  
   protected
- 
+
   def sign_up_params
 
     params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation)
   end
- 
+
   def account_update_params
     params.require(:user).permit(:firstName, :lastName, :email, :password, :password_confirmation, :current_password)
   end
